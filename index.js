38e19cf37d9b425d3dfb17738723f02b720ff96a
Guilherme Mattoso <guilhermemktfran@gmail.com>
2022-06-04 02:12:27 -0300

    commit

--- index.js
+++ index.js
@@ -1,26 +1,66 @@
 // Importa o módulo express para esse arquivo
 const express = require("express");
-const { url } = require("inspector");
+const nodemailer = require("nodemailer");
+
+
 // Instancia uma referência do express no projeto
 const app = express();
 const port = process.env.PORT || 3010; // Const para armanezar a porta do servidor
 app.set("view engine", "ejs");
-const path = require("path");
-app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static(__dirname + "/assets", {
-  index: false, 
-  immutable: true, 
-  cacheControl: true,
-  maxAge: "30d"
-}));
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  express.static(__dirname + "/assets", {
+    index: false,
+    immutable: true,
+    cacheControl: true,
+    maxAge: "30d",
+  })
+);
 // Liga o servidor na porta 3000
 //app.listen(3000);
 
-
 app.get("/", (req, res) => {
-    res.render("index");
+  res.render("index");
 });
 
+app.get("/send", async (req, res)  => {
 
-app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
+  const user = {
+    nome: "Guilherme Mattoso",
+    email: "guilhermemktfran@gmail.com",
+    assunto: "Teste",
+    mensagem: "Isso é um teste",
+  };
+  const transporter = nodemailer.createTransport({
+    // host: process.env.MAIL_HOST,
+    port: Number(process.env.PORT),
+    service: 'gmail',
+    secure: false,
+    secureConnection: false,
+    auth: {
+      user: process.env.USER,
+      pass:process.env.PASS,
+    },
+  });
+
+  console.log(process.env.USER);
+
+  const mail = await transporter.sendMail({
+    from: user.email,
+    to: "ivanpintoadvocacia@gmail.com",
+    subject: user.assunto,
+    text: user.mensagem,
+  }).catch(e => console.log(e))
+
+  res.send(mail)
+  console.log(mail);
+  
+});
+
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
