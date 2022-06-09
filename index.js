cf7eb56072c9a0d5d554677fbbf7e577dc5e8f83
FredyyLima <fredyy_lima@live.com>
2022-06-09 00:31:42 -0300

    atualização

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 // Instancia uma referência do express no projeto
 const app = express();
-const port = process.env.PORT || 3010; // Const para armanezar a porta do servidor
+const port = process.env.PORT || 3000; // Const para armanezar a porta do servidor
 app.set("view engine", "ejs");
 const path = require("path");
 app.use(express.urlencoded({ extended: true }))
@@ -23,23 +23,67 @@ app.use(express.static(__dirname + "/assets", {
 // Liga o servidor na porta 3000
 //app.listen(3000);
 
-// var loading = document.getElementsByClassName('loading')
-// var error = document.getElementsByClassName('error-message')
-// var sent = document.getElementsByClassName('sent-message')
+// var loading = document.getElementsByid('alerta')
+// var error = document.getElementsByid('alerta')
+// var sent = document.getElementsByid('alerta')
+let emailAtivo =''
+let senhaAtiva = ''
 
 app.get("/", (req, res) => {
     res.render("index");
 });
 
 app.post("/send", async (req,res) => {
+
+  const {name, email, message, atuacao} = req.body;
+  let user = {
+    nome: name,
+    email: email,
+    mensagem: message,
+    atuacao: atuacao
+  }
+
+  let update =''
+  
+  if (atuacao === 'civel'){
+    update ={
+      atuacao: "Direito Cível"
+    }
+    emailAtivo = process.env.MAIL_USER_IVAN
+    senhaAtiva = process.env.MAIL_PASS_IVAN
+  } if (atuacao === 'criminal') {
+    update ={
+      atuacao: "Direito Criminal"
+    }
+    emailAtivo = process.env.MAIL_USER_HERLON
+    senhaAtiva = process.env.MAIL_PASS_HERLON
+  } if (atuacao === 'previdenciario') {
+    update = {
+      atuacao: "Direito Previdenciário"
+    }
+    emailAtivo = process.env.MAIL_USER_HUELTON
+    senhaAtiva = process.env.MAIL_PASS_HUELTON
+  } if (atuacao === 'trabalhista') {
+    update = {
+      atuacao: "Direito Trabalhista"
+    }
+    emailAtivo = process.env.MAIL_USER_IVAN
+    senhaAtiva = process.env.MAIL_PASS_IVAN
+  }
+
+
+ Object.assign(user, update);
+
+
+
     const transporter = nodemailer.createTransport( new smtpTransport({
         
             host: process.env.MAIL_HOST,
             port: Number(process.env.MAIL_PORT),
             // service:"yahoo",
             auth: {
-              user: process.env.MAIL_USER,
-              pass: process.env.MAIL_PASS,
+              user: emailAtivo,
+              pass: senhaAtiva,
             },
             
             // debug: true,
@@ -54,22 +98,12 @@ app.post("/send", async (req,res) => {
           
     }));
 
-    // const {name, email, subject, message} = req.body;
-    // const user = {
-    //   nome: name,
-    //   email: email,
-    //   assunto: subject,
-    //   mensagem: message
-    // }
-    const user = req.body
-    console.log(user)
-  
     await transporter
      .sendMail({
       
         from: `${user.nome} <${user.email}> `,
-        to: process.env.MAIL_USER,
-        subject: `${user.assunto}`,
+        to: emailAtivo,
+        subject: `${user.atuacao}`,
         text: `
         Cliente: ${user.nome}
         Contato: ${user.email}
@@ -80,7 +114,7 @@ app.post("/send", async (req,res) => {
       .then((r) => {
           // loading.innerHTML = 'Carregando'
           console.log(r);
-          // sent.innerHTML = `${user.name}, sua mensagem foi enviada com sucesso! Logo entraremos em contato.`
+          // sent.innerHTML = `<div class="sent-message">Your message has been sent.Thank you!</div>`
           res.redirect('/');
           
       })
